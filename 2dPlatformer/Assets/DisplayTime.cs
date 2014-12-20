445ed993ef5b59202f690a6f48b7b137d64eeeba
eshalkowski <eshalkowski@gmail.com>
2014-12-20 03:17:34 -0500

    Added Timer

---
+++ 2dPlatformer/Assets/DisplayTime.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayTime : MonoBehaviour {
+	private KeepTime timer;
+	private GUIStyle styleTime;
+	private GUIStyle styleLabel;
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.Find ("Game");
+		timer = go.GetComponent <KeepTime> ();
+		styleLabel = new GUIStyle();
+		styleTime = new GUIStyle();
+		styleTime.fontSize= 33;
+		styleLabel.fontSize = 55;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer.getTime();
+	}
+	void OnGUI() {
+		string time = "" + (int)(timer.getTime());
+		GUI.Label(new Rect(10, 10, 100, 20),"Time" , styleLabel);
+		GUI.Label(new Rect(75, 75, 100, 20),time , styleTime);
+	}
+}
---
+++ 2dPlatformer/Assets/KeepTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepTime : MonoBehaviour {
+	private double gameTime;
+	// Use this for initialization
+	void Start () {
+		gameTime = 0.0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameTime += Time.deltaTime;
+		Debug.Log (gameTime);
+	}
+	public double getTime(){
+		return gameTime;
+	}
+}
